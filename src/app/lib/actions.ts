bcecac38ad17a40aa456318e1ec87712d89c684c
Idrees Khan <khanidrees1234@gmail.com>
2025-07-13 07:41:51 +0000

    learner bug fixed

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -231,7 +231,8 @@ export async function addLearner(prevState : learnerAddState, formData: FormData
     if (!course) return { message: 'Course not found', status: 404 };
     let user = await User.findOne({ username });
     if (!user) {
-      user = await User.create({  learnerName, username, role: 'learner', password: 'Password123' });
+      const hashedPassword = await bcrypt.hash( 'Password123',10 );
+      user = await User.create({  learnerName, username, role: 'learner', password: hashedPassword });
     }else if(course.learners.includes(user._id)){
       return { message: 'Learner already added', status: 409 };  
     }
